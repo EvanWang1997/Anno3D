8a287d8548a529d608ef677d1f2a6c572fa2e94d
Evan Wang <evan7@students.cs.ubc.ca>
2020-10-29 10:23:47 -0700

    Initial commit

---
+++ app/src/main/java/com/example/anno3d/Scroll_File.java
@@ -0,0 +1,14 @@
+package com.example.anno3d;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Scroll_File extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scroll__file);
+    }
+}
